f6f6978e4876b32edee9f9fe659e8d1572691bfe
FaraBro <you.denis.bro@gmail.com>
2026-04-08 17:37:24 +0500

    Обновление turbine.lua, README.
    Добавлено:
     • Режимы работы АСУ ТП в настройках
     • Исправлен баг с перегревом турбины если выключить вручную турбину. Теперь при ручном выключении АСУ ТП идёт в cooldown на ~30 секунд.
    
     Режим работы АСУ ТП - 2:
      Работает как надзиратель за электросетью. Включает газовые турбины комб. цикла при недостатке электропитания и управляет мощностью их, а также выключает турбины если питание возобновлено.
     • Добавлено автоматическое обнаружение аккумуляторов энергосети.
     • Добавлено плавное, линейное управление мощности при заполненности аккумуляторов 50-90%. При показателях ниже 50% работает на полную мощность, при выше 90% работает в холостую, на 95% выключается.

--- turbine.lua
+++ turbine.lua
@@ -2,10 +2,18 @@
 settings = {
 	outputInfo_To_Console = false, -- Вывод информации о турбинах в консоль, может снизить производительность системы и повысить потребление энергии при включении
 	AutoSearchTurbines = true, -- Автоматический поиск турбин комб. цикла
+	mode = { -- Режим работы
+		mode = 1, -- Номер режима работы
+		
+		-- Настройки режима работы 2
+		AutoSearchEnergyStorages = true, -- Автоматический поиск аккумуляторов
+		EnergySrorages_address = { -- Адресы аккумуляторов (не используется, если включён автоматический поиск). Указывается в таком же формате как и адресы турбин комб. цикла.
+		},
+	},
 }
 
 -- Адресы турбин
-turbines_address = {
+turbines_address = { --Адресы турбин комб. цикла (не используется, если включён автоматический поиск)
 	'', -- Адрес турбины №1
 	'', -- Адрес турбины №2
 	'', -- и так далее P.S. можно от 0 до Inf турбин (ограничивается сервером OpenComputers и вашим компьютером)
@@ -14,31 +22,88 @@ turbines_address = {
 timeOfSleep = 2.5 -- Время ожидания после цикла
 
 component = require('component')
+computer = require('computer')
 
 -- Импортируем адресы турбин
 turbines = {}
+turbines_data = {}
 if (settings.AutoSearchTurbines==false) then
 	for index, value in ipairs(turbines_address) do
 		turbines[index] = component.proxy(component.get(value))
+		turbines_data[index] = {}
+		turbines_data[index]['Must_work'] = false
 	end
 else
 	numb = 1
 	turbines_list = component.list("ntm_gas_turbine")
 	for key, value in pairs(turbines_list) do
 		turbines[numb] = component.proxy(key)
+		turbines_data[numb] = {}
+		turbines_data[numb]['Must_work'] = false
+		
 		numb = numb + 1
+	end
+end
+
+if settings.mode.mode == 2 then
+	-- Импортируем адресы аккумуляторов
+	EnergySrorages = {}
+	if (settings.mode.AutoSearchEnergyStorages==false) then
+		for index, value in ipairs(settings.mode.EnergySrorages_address) do
+			EnergySrorages[index] = component.proxy(component.get(value))
+		end
+	else
+		numb = 1
+		EnergySrorages_list = component.list("ntm_energy_storage")
+		for key, value in pairs(EnergySrorages_list) do
+			EnergySrorages[numb] = component.proxy(key)
+			numb = numb + 1
+		end
 	end
 end
 
 -- Основной цикл
 
 while true do
+	if settings.mode.mode == 2 then
+		-- Импортируем данные аккумуляторов
+		
+		EnergySrorages_data = {}
+		for index, value in ipairs(EnergySrorages) do
+			EnergySrorage_data = {}
+			EnergySrorage_data['Enow'], EnergySrorage_data['Emax'], EnergySrorage_data['upd'], EnergySrorage_data['redact_noRed'], EnergySrorage_data['redact_withRed'], EnergySrorage_data['priority'], EnergySrorage_data['type'], EnergySrorage_data['Emax_in'], EnergySrorage_data['Emax_out']  = value.getInfo()
+			EnergySrorage_data['Enow_in_prosent'] = EnergySrorage_data['Enow']/EnergySrorage_data['Emax']*100
+			
+			EnergySrorages_data[index] = EnergySrorage_data
+		end
+		
+		-- Действия с аккумуляторами
+		MaxEStorage = 0
+		NowESrorage = 0
+		for index, value in ipairs(EnergySrorages_data) do
+			MaxEStorage = MaxEStorage + value['Emax']
+			NowESrorage = NowESrorage + value['Enow']
+			if value['redact_noRed'] ~= 1 then
+				EnergySrorages[index].setModeLow(1)
+				computer.beep()
+			end
+			if value['redact_withRed'] ~= 1 then
+				EnergySrorages[index].setModeHigh(1)
+				computer.beep()
+			end
+			if value['priority'] ~= 0 then
+				EnergySrorages[index].setPriority(0)
+				computer.beep()
+			end
+		end
+		EnergyAVprosent = NowESrorage/MaxEStorage*100
+		Throttle = 225-2.5*EnergyAVprosent
+	end
 	
 	-- Импортируем данные турбин
 	
-	turbines_data = {}
 	for index, value in ipairs(turbines) do
-		turbine_data = {}
+		turbine_data = turbines_data[index]
 		turbine_data['power'], turbine_data['status'], turbine_data['fuel'], turbine_data['max_fuel'], turbine_data['lubricant'], turbine_data['max_lubricant'], turbine_data['water'], turbine_data['max_water'], turbine_data['steam'], turbine_data['max_steam'] = value.getInfo() -- Получаем данные с турбины
 		
 		turbines_data[index] = turbine_data
@@ -47,11 +112,46 @@ while true do
 	-- Действия с турбинами
 	
 	for index, value in ipairs(turbines_data) do
-		if value['status'] == 0 and value['fuel'] > 0 and value['lubricant'] > 0 then
-			turbines[index].start()
+		if settings.mode.mode == 1 then
+			if value['status'] == 0 and value['fuel'] > 0 and value['lubricant'] > 0 then
+				if value['Must_work'] == false then
+					turbines[index].start()
+					turbines_data[index]['Must_work'] = true
+				else
+					computer.beep(1750, 30)
+					os.sleep(20)
+					turbines_data[index]['Must_work'] = false
+				end
+			end
+			if value['power'] < 100 and value['status'] == 1 then
+				turbines[index].setThrottle(100)
+			end
+		elseif settings.mode.mode == 2 then
+			if value['status'] == 0 then
+				if EnergyAVprosent < 75 and value['fuel'] > 0 and value['lubricant'] > 0 then
+					if value['Must_work'] == false then
+						turbines[index].start()
+						turbines_data[index]['Must_work'] = true
+					else
+						computer.beep(1750, 30)
+						os.sleep(20)
+						turbines_data[index]['Must_work'] = false
+					end
+				end
+			elseif value['status'] == 1 then
+				if EnergyAVprosent > 95 then
+					turbines[index].stop()
+					turbines_data[index]['Must_work'] = false
+				end
+				if EnergyAVprosent < 50 then
+					turbines[index].setThrottle(100)
+				else
+					turbines[index].setThrottle(Throttle)
+				end
+			end
 		end
-		if value['power'] < 100 and value['status'] == 1 then
-			turbines[index].setThrottle(100)
+		if value['fuel'] < 0 or value['lubricant'] < 0 then
+			turbines_data[index]['Must_work'] = false
 		end
 	end
 	
@@ -82,6 +182,15 @@ while true do
 				print("	Статус: 	Запускается")
 			end
 			print()
+		end
+		
+		if settings.mode.mode == 2 then
+			for index, value in ipairs(EnergySrorages_data) do
+				print("Энергохранилище ", index)
+				print("	Энергия: ", value['Enow'], " HE / ", value['Emax'], " HE (", string.format("%.2f" ,value['Enow_in_prosent'])," %)")
+				print()
+			end
+			print("Общее наполнение аккумуляторов: ", NowESrorage, "HE / ", MaxEStorage, " HE (", string.format("%.2f" ,EnergyAVprosent)," %)")
 		end
 	end
 	
